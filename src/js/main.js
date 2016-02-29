c69d12ae61f0a16ba61f925f3ab40db087659050
Arjan Ceelen <arjan.ceelen@indivirtual.com>
2016-02-29 17:01:57 +0100

    - Added ES6 polyfill for older browsers.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import "babel-polyfill"; // polyfill for ES6 in the browser. This will emulate a full ES6 environment.
 import ExampleModule from './modules/examplemodule';
 
 document.addEventListener('DOMContentLoaded', function(){
